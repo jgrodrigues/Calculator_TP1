f606dda7e9fbca92490cde298b6fb498aa98e895
Jonas Rodrigues <jonas@10-22-213-242.ed7.openfct.fct.unl.pt>
2016-11-03 11:25:46 +0000

    VM and LM

--- Calculator.java
+++ Calculator.java
@@ -4,10 +4,91 @@
 public class Calculator {
 
     //Variaveis
+    private double memory1;
+    private double memory2;
+    private String memory1Name;
+    private String memory2Name;
+    private boolean hasMemory = false;
+    private int memoryNumber;
 
     //Constructor
-    public Calculator() {
-//        memory1 = 0;
-//        memory2 = 0;
+    public Calculator(String memory1Name, String memory2Name) {
+        if (memory1Name.equals(memory2Name)) {
+            this.memory1Name = memory1Name;
+            memory1 = 0;
+            hasMemory = true;
+            memoryNumber = 1;
+
+        }else {
+            this.memory1Name = memory1Name;
+            this.memory2Name = memory2Name;
+            memory1 = 0;
+            memory2 = 0;
+            hasMemory = true;
+            memoryNumber = 2;
+        }
     }
+
+    public Calculator (String memory1Name) {
+        this.memory1Name = memory1Name;
+        memory1 = 0;
+        memoryNumber = 2;
+        hasMemory = true;
+    }
+
+
+    public Calculator () {
+        memoryNumber = 0;
+    }
+
+    public boolean getHasMemory() {
+        return hasMemory;
+    }
+
+    public int getMemoryNumber() {
+        return memoryNumber;
+    }
+
+
+    /**
+     * RETURN VALUE OF NAMED MEMORY
+     *
+     * @param memoryName
+     * @return
+     */
+    public double getMemoryValue(String memoryName) {
+        double value;
+        if (memoryName.equals(memory1Name)) {
+            value = memory1;
+        } else if(memoryName.equals(memory2Name)) {
+            value = memory2;
+        } else {
+            value = 0;
+        }
+        return value;
+    }
+
+    public boolean memoryExists(String memoryName) {
+        return (memoryName.equals(memory1Name)  || memoryName.equals(memory2Name));
+    }
+
+    public String[] getMemoryNames() {
+        String[] arr;
+        for(int i = 0; i < 2;i++) {
+            arr = new String[]{memory1Name, memory2Name};
+            return arr;
+        }
+        arr = new String[]{memory1Name, memory2Name};
+        return arr;
+
+    }
+
+//    public double simpleExpression () {
+//
+//    }
+
+//    public double complexExpression() {
+//
+//    }
+
 }
--- Main.java
+++ Main.java
@@ -13,7 +13,7 @@ public class Main {
         if (input.charAt(lastParentheses + 1) == '+') {
             exp = input.substring((lastParentheses + 1), (input.indexOf(')', lastParentheses + 1)));
             System.out.println(exp);
-        } else if(Character.isDigit(input.charAt(lastParentheses + 1))) {
+        } else if (Character.isDigit(input.charAt(lastParentheses + 1))) {
             System.out.println("ya");
             //parcelaDireita = input.subs
 
@@ -23,21 +23,140 @@ public class Main {
         return 0;
     }
 
-    private static
+    private static void handleExpression(Scanner in, Calculator c1) {
+
+    }
+
+    /**
+     * SHOW HELP MENU
+     */
+    private static void showOptions() {
+        //MENU
+    }
+
+    /**
+     * VM - Consult Memory Value - WORKING!
+     * @param memoryName
+     * @param c1
+     */
+    private static void consultMemoryValue(String memoryName, Calculator c1) {
+        if (c1.memoryExists(memoryName)) {
+            System.out.printf("%.2f\n", c1.getMemoryValue(memoryName));
+        } else {
+            System.out.println("Memoria nao existente");
+        }
+    }
+
+    private static void lm (Calculator c1) {
+        if(c1.getHasMemory()) {
+            for (int i = 0; i < c1.getMemoryNames().length; i++) {
+                if (!c1.getMemoryNames()[i].isEmpty()) {
+                    System.out.println(c1.getMemoryNames()[i] + ": " + c1.getMemoryValue(c1.getMemoryNames()[i]));
+                }
+            }
+        } else {
+            System.out.println("Calculadora sem memorias.");
+        }
+    }
 
 
+    private static void consultMemoryName(Calculator c1) {
+        if (c1.getHasMemory()) {
+
+        }
+    }
+
+    /**
+     * ASSIGN MEMORY NAMES - WORKING!!
+     *
+     * @param in
+     */
+    private static void assignMemory(Scanner in) {
+        String[] memories = readLine(in).toUpperCase().split(" ");
+        Calculator c1 = new Calculator(memories[0], memories[1]);
+    }
+
+
+    private static String readLine(Scanner in) {
+        return in.nextLine().toUpperCase();
+    }
+
+    private static String readNext(Scanner in) {
+        return in.next().toUpperCase();
+    }
 
     public static void main(String[] args) {
-        Calculator calc1 = new Calculator();
+
+
         Scanner in = new Scanner(System.in);
         Counter count1 = new Counter();
+        Calculator c1;
 
-        String input = in.next();
-        char[] arr = input.toCharArray();
+        System.out.println("Insira nome de cada uma das duas memorias(ex: primeira segunda): ");
+        String[] memories = {};
+        String start = readLine(in);
 
-        checkLastExpression(input);
 
-        in.nextLine();
+
+        if (!start.isEmpty()) {
+            memories = start.split("\\s+");
+            if (memories.length == 2) {
+                c1 = new Calculator(memories[0], memories[1]);
+            } else if(memories.length == 1) {
+                c1 = new Calculator(memories[0]);
+            } else {
+                c1 = new Calculator();
+            }
+        } else{
+            c1 = new Calculator();
+        }
+
+        System.out.println(Arrays.toString(memories));
+
+
+        String option = in.next().toUpperCase();
+
+        while(!option.equals("S")) {
+
+            switch (option) {
+                case ("VM"): //consultar valor memoria
+                    consultMemoryValue(in.next().toUpperCase(), c1);
+                    break;
+                case ("LM"): //informa nome das memorias com valor 2 casas
+                    lm(c1);
+                    break;
+                default:
+                    //                System.out.println(option);
+                    System.out.println("Greg");
+                    break;
+            }
+            in.nextLine();
+            option = in.next().toUpperCase();
+        }
+    }
+}
+
+
+
+
+
+//        handleExpression(in, c1);
+
+
+//        String memArr[] = input.split(" ");
+
+
+//        System.out.println(Arrays.toString(memArr));
+
+
+
+//        char[] arr = input.toCharArray();
+
+
+
+
+        //checkLastExpression(input);
+
 
 
 //
@@ -111,7 +230,5 @@ public class Main {
 
 
 
-    }
-}
 
 
